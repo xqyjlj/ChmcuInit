2c9e113cea8590669439ae977b9ae874b3b3d0ab
xqyjlj <xqyjlj@126.com>
2021-02-13 17:35:42 +0800

    更新

---
+++ ChmcuInit/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using ChmcuInit.Views;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using System;
+
+namespace ChmcuInit.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "ChMcuInit";
+
+        public DelegateCommand OpenStartViewCommand { get;private set; }
+
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private readonly IRegionManager regionManger;
+
+        public MainWindowViewModel(IRegionManager regionManger)
+        {
+            this.regionManger = regionManger;
+
+            OpenStartViewCommand = new DelegateCommand(OpenStartView);
+
+            regionManger.RegisterViewWithRegion("ContentRegion", typeof(StartView));
+        }
+
+        private void OpenStartView()
+        {
+            regionManger.RequestNavigate("ContentRegion", "StartView");
+        }
+
+    }
+}
