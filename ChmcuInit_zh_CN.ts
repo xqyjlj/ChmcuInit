a2f55dc2c28472f9564a9f0c4ee0c387aa941a5e
小权一句两句 <xqyjlj@126.com>
2021-03-12 01:09:21 +0800

    为了跨平台，还是选用了qt

---
+++ ChmcuInit_zh_CN.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename=".build/ChmcuInit_autogen/include/ui_mainwindow.h" line="50"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
